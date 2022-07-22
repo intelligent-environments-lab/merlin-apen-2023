b1cb9f6f312766acd079637e970c13c4b848a101
Kingsley Nweye <etonwana@yahoo.com>
2022-07-22 13:31:48 -0500

    updated

--- src/experiment.py
+++ src/experiment.py
@@ -36,12 +36,14 @@ def run(experiment):
                 print(e)
 
 def set_hyperparameter_design_work_order():
+    experiment = 'hyperparameter_design'
     kwargs = preliminary_setup()
     schema = kwargs['schema']
     schema_directory = kwargs['schema_directory']
     src_directory = kwargs['src_directory']
     misc_directory = kwargs['misc_directory']
     work_order_directory = kwargs['work_order_directory']
+    tacc_directory = kwargs['tacc_directory']
     settings = get_settings()
 
     # set active buildings
@@ -51,10 +53,10 @@ def set_hyperparameter_design_work_order():
         schema['buildings'][building]['include'] = True if int(building.split('_')[-1]) in train_buildings else False
 
     # set reward
-    schema['reward'] = settings['hyperparameter_design']['reward']
+    schema['reward'] = settings[experiment]['reward']
 
     # hyperparameter definition
-    hyperparameter_grid = settings['hyperparameter_design']['grid']
+    hyperparameter_grid = settings[experiment]['grid']
     hyperparameter_grid['seed'] = settings['seeds']
     param_names = list(hyperparameter_grid.keys())
     param_values = list(hyperparameter_grid.values())
@@ -62,13 +64,14 @@ def set_hyperparameter_design_work_order():
     grid = pd.DataFrame(param_values_grid,columns=param_names)
     grid = grid.sort_values(['seed'])
     grid['buildings'] = str(train_buildings)
-    grid['simulation_id'] = grid.reset_index().index.map(lambda x: f'hyperparameter_design_{x}')
-    grid.to_csv(os.path.join(misc_directory,'hyperparameter_design_grid.csv'),index=False)
+    grid['simulation_id'] = grid.reset_index().index.map(lambda x: f'{experiment}_{x}')
+    grid.to_csv(os.path.join(misc_directory,f'{experiment}_grid.csv'),index=False)
 
     # design work order
     work_order = []
 
     for i, params in enumerate(grid.to_dict('records')):
+        params['seed'] = int(params['seed'])
         schema['agent']['attributes'] = {
             **schema['agent']['attributes'],
             **params
@@ -77,15 +80,18 @@ def set_hyperparameter_design_work_order():
         write_json(schema_filepath, schema)
         work_order.append(f'python {os.path.join(src_directory,"simulate.py")} {schema_filepath} {params["simulation_id"]}')
 
+    # write work order and tacc job
     work_order = '\n'.join(work_order)
-    work_order_filepath = os.path.join(work_order_directory,'hyperparameter_design.sh')
-    
-    with open(work_order_filepath,'w') as f:
-        f.write(work_order)
+    tacc_job = get_tacc_job(experiment)
+    work_order_filepath = os.path.join(work_order_directory,f'{experiment}.sh')
+    tacc_job_filepath = os.path.join(tacc_directory,f'{experiment}.sh')
 
-    return work_order_filepath
+    for d, p in zip([work_order,tacc_job],[work_order_filepath,tacc_job_filepath]):
+        with open(p,'w') as f:
+            f.write(d)
 
 def set_rbc_validation_work_order():
+    experiment = 'rbc_validation'
     kwargs = preliminary_setup()
     schema = kwargs['schema']
     schema_directory = kwargs['schema_directory']
@@ -97,16 +103,16 @@ def set_rbc_validation_work_order():
     # update general settings
     schema['simulation_end_time_step'] = settings["test_end_time_step"]
     schema['episodes'] = 1
-    grid = pd.DataFrame({'type':settings['rbc_validation']['type']}) 
-    grid['simulation_id'] = grid.reset_index().index.map(lambda x: f'rbc_validation_{x}')
-    grid.to_csv(os.path.join(misc_directory,'rbc_validation_grid.csv'),index=False)
+    grid = pd.DataFrame({'type':settings[experiment]['type']}) 
+    grid['simulation_id'] = grid.reset_index().index.map(lambda x: f'{experiment}_{x}')
+    grid.to_csv(os.path.join(misc_directory,f'{experiment}_grid.csv'),index=False)
     work_order = []
 
     # update agent
     for i, params in enumerate(grid.to_records(index=False)):
         schema['agent'] = {
             'type': params['type'],
-            'attributes': settings['rbc_validation']['attributes']
+            'attributes': settings[experiment]['attributes']
             **{
                 'hour_index':settings['observations'].index('hour'),
                 'soc_index':settings['observations'].index('electrical_storage_soc'),
@@ -119,7 +125,7 @@ def set_rbc_validation_work_order():
 
     # write work order
     work_order = '\n'.join(work_order)
-    work_order_filepath = os.path.join(work_order_directory,'rbc_validation.sh')
+    work_order_filepath = os.path.join(work_order_directory,f'{experiment}.sh')
 
     with open(work_order_filepath,'w') as f:
         f.write(work_order)
@@ -128,12 +134,14 @@ def set_rbc_validation_work_order():
 
 def set_reward_design_work_order():
     # buildings to include
+    experiment = 'reward_design'
     kwargs = preliminary_setup()
     schema = kwargs['schema']
     misc_directory = kwargs['misc_directory']
     schema_directory = kwargs['schema_directory']
     src_directory = kwargs['src_directory']
     work_order_directory = kwargs['work_order_directory']
+    tacc_directory = kwargs['tacc_directory']
     settings = get_settings()
     train_buildings = settings['design_buildings']
     
@@ -141,7 +149,7 @@ def set_reward_design_work_order():
         schema['buildings'][building]['include'] = True if int(building.split('_')[-1]) in train_buildings else False
 
     # reward definition
-    reward_design_grid = settings['reward_design']['grid']
+    reward_design_grid = settings[experiment]['grid']
     reward_design_grid['seed'] = settings['seeds']
     param_names = list(reward_design_grid.keys())
     param_values = list(reward_design_grid.values())
@@ -149,8 +157,8 @@ def set_reward_design_work_order():
     grid = pd.DataFrame(param_values_grid,columns=param_names)
     grid = grid.sort_values(['seed','weight','exponent'])
     grid['buildings'] = str(train_buildings)
-    grid['simulation_id'] = grid.reset_index().index.map(lambda x: f'reward_function_{x}')
-    grid.to_csv(os.path.join(misc_directory,'reward_design_grid.csv'),index=False)
+    grid['simulation_id'] = grid.reset_index().index.map(lambda x: f'{experiment}_{x}')
+    grid.to_csv(os.path.join(misc_directory,f'{experiment}_grid.csv'),index=False)
 
     # design work order
     work_order = []
@@ -165,39 +173,42 @@ def set_reward_design_work_order():
                 'carbon_emission_exponent': float(params['exponent']),
             }  
         }
-        schema['agent']['attributes']['seed'] = params['seed']
+        schema['agent']['attributes']['seed'] = int(params['seed'])
         schema_filepath = os.path.join(schema_directory,f'{params["simulation_id"]}.json')
         write_json(schema_filepath, schema)
         work_order.append(f'python {os.path.join(src_directory,"simulate.py")} {schema_filepath} {params["simulation_id"]}')
 
-    # write work order
+    # write work order and tacc job
     work_order = '\n'.join(work_order)
-    work_order_filepath = os.path.join(work_order_directory,'reward_design.sh')
-    
-    with open(work_order_filepath,'w') as f:
-        f.write(work_order)
+    tacc_job = get_tacc_job(experiment)
+    work_order_filepath = os.path.join(work_order_directory,f'{experiment}.sh')
+    tacc_job_filepath = os.path.join(tacc_directory,f'{experiment}.sh')
 
-    return work_order_filepath
+    for d, p in zip([work_order,tacc_job],[work_order_filepath,tacc_job_filepath]):
+        with open(p,'w') as f:
+            f.write(d)
 
 def preliminary_setup():
     # set filepaths and directories
     root_directory = os.path.join(*Path(os.path.dirname(__file__)).absolute().parts[0:-1])
     src_directory = os.path.join(*Path(os.path.dirname(__file__)).absolute().parts)
+    data_set_directory = os.path.join(root_directory,'data','citylearn_challenge_2022_phase_3')
     schema_directory = os.path.join(root_directory,'data','schema')
     work_order_directory = os.path.join(root_directory,'data','work_order')
     misc_directory = os.path.join(root_directory,'data','misc')
+    tacc_directory = os.path.join(root_directory,'tacc')
     os.makedirs(schema_directory,exist_ok=True)
     os.makedirs(work_order_directory,exist_ok=True)
     os.makedirs(misc_directory,exist_ok=True)
+    os.makedirs(tacc_directory,exist_ok=True)
 
     # general simulation settings
     settings = get_settings()
-    schema = read_json(settings['schema_filepath'])
+    schema = read_json(os.path.join(data_set_directory,'schema.json'))
     schema['simulation_start_time_step'] = settings["train_start_time_step"]
     schema['simulation_end_time_step'] = settings["train_end_time_step"]
     schema['episodes'] = settings["train_episodes"]
-    schema['root_directory'] = os.path.join(*Path(settings['schema_filepath']).parts[0:-1])
-
+    schema['root_directory'] = data_set_directory
     # set active observations
     for o in schema['observations']:
         schema['observations'][o]['active'] = True if o in settings['observations'] else False
@@ -211,7 +222,8 @@ def preliminary_setup():
         'src_directory': src_directory, 
         'schema_directory': schema_directory, 
         'work_order_directory': work_order_directory, 
-        'misc_directory': misc_directory
+        'misc_directory': misc_directory,
+        'tacc_directory': tacc_directory,
     }
 
 def get_settings():
@@ -219,6 +231,36 @@ def get_settings():
     settings_filepath = os.path.join(src_directory,'settings.json')
     settings = read_json(settings_filepath)
     return settings
+
+def get_tacc_job(experiment):
+    settings = get_settings()
+    queue = settings[experiment]['tacc_queue']
+    nodes = settings['tacc_queue'][queue]['nodes']
+    time = settings['tacc_queue'][queue]['time']
+    return '\n'.join([
+        '#!/bin/bash',
+        f'#SBATCH -p {queue}',
+        f'#SBATCH -J citylearn_buildsys_2022_{experiment}',
+        f'#SBATCH -N {nodes}',
+        '#SBATCH --tasks-per-node 1',
+        f'#SBATCH -t {time}',
+        '#SBATCH --mail-user=nweye@utexas.edu',
+        '#SBATCH --mail-type=all',
+        f'#SBATCH -o slurm_{experiment}.out',
+        '#SBATCH -A DemandAnalysis',
+        '',
+        '# load modules',
+        'module load launcher',
+        '',
+        '# activate virtual environment',
+        'source /work/07083/ken658/projects/citylearn/buildsys_2022/env/bin/activate',
+        '',
+        '# set launcher environment variables',
+        'export LAUNCHER_WORKDIR="/work/07083/ken658/projects/citylearn/buildsys_2022/data/work_order"',
+        f'export LAUNCHER_JOB_FILE="{experiment}.sh"',
+        '',
+        '${LAUNCHER_DIR}/paramrun',
+    ])
 
 def set_work_order(experiment):
     func = {
