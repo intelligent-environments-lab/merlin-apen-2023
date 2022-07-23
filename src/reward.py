2648c71bd9ce38c84150aeb9d072cf82440a1766
Kingsley Nweye <etonwana@yahoo.com>
2022-07-23 11:39:37 -0500

    added solar penalty reward

--- src/reward.py
+++ src/reward.py
@@ -21,3 +21,14 @@ class AdditiveReward(RewardFunction):
         electricity_price = (np.array(self.electricity_price).clip(min=0)*self.kwargs['electricity_price_weight'])**self.kwargs['electricity_price_exponent']
         reward = -(carbon_emission + electricity_price)
         return reward
+
+class AdditiveSolarPenaltyReward(RewardFunction):
+    def __init__(self, electricity_consumption: List[float] = None, **kwargs):
+        super().__init__(electricity_consumption=electricity_consumption, **kwargs)
+
+    def calculate(self) -> List[float]:
+        carbon_emission = (np.array(self.carbon_emission)*self.kwargs['carbon_emission_weight'])**self.kwargs['carbon_emission_exponent']
+        electricity_price = (np.array(self.electricity_price)*self.kwargs['electricity_price_weight'])**self.kwargs['electricity_price_exponent']
+        soc = self.kwargs.get('electrical_storage_soc', np.array([0.0]*self.agent_count))
+        reward = -(1.0 + np.sign(electricity_price)*soc)*abs(carbon_emission + electricity_price)
+        return reward
