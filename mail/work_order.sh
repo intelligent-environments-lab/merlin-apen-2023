169eaa3f9ea90f0778179feedf87202960828eea
Kingsley Nweye <etonwana@yahoo.com>
2022-08-18 09:31:22 -0500

    deleted mail

--- mail/work_order.sh
+++
@@ -1,3 +0,0 @@
-python /work2/07083/ken658/projects/citylearn/buildsys_2022/src/simulate.py /work2/07083/ken658/projects/citylearn/buildsys_2022/data/schema/solar_reward.json solar_reward_3_0_0 --recalculate_reward
-python /work2/07083/ken658/projects/citylearn/buildsys_2022/src/simulate.py /work2/07083/ken658/projects/citylearn/buildsys_2022/data/schema/ramping_reward.json ramping_reward_3_0_0
-python /work2/07083/ken658/projects/citylearn/buildsys_2022/src/simulate.py /work2/07083/ken658/projects/citylearn/buildsys_2022/data/schema/battery_rbc.json battery_rbc_3_0_0
